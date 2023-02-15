a0b921a173ecedd25eb6303b7f3e7b2754f08b1b
Eduardo Worrel <eduardo.worrel@gmail.com>
2023-02-14 22:00:28 -0300

    aula2.1

---
+++ chat/Receive/Program.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+var factory = new ConnectionFactory() { 
+    HostName = "142.93.173.18",
+    UserName = "admin",
+    Password = "devintwitter"
+};
+using(var connection = factory.CreateConnection())
+using(var channel = connection.CreateModel())
+{
+
+    var consumerA = new EventingBasicConsumer(channel);
+    
+    var consumerB = new EventingBasicConsumer(channel);
+    
+                            
+    consumerA.Received += (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine(" [A] Received {0}", message);
+    };
+    consumerB.Received += (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine(" [B] Received {0}", message);
+    };
+
+
+    channel.BasicConsume(queue: "helloA",
+                            autoAck: true,
+                            consumer: consumerA);
+
+    channel.BasicConsume(queue: "helloB",
+                            autoAck: true,
+                            consumer: consumerB);
+
+    Console.WriteLine(" Press [enter] to exit.");
+    Console.ReadLine();
+}
---
+++ chat/Send/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory() { 
+    HostName = "142.93.173.18",
+    UserName = "admin",
+    Password = "devintwitter"
+};
+try{
+    
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+    channel.ExchangeDeclare("chat", ExchangeType.Fanout);
+
+    channel.QueueDeclare(queue: "chat-eduardoworrel",
+                            durable: false,
+                            exclusive: false,
+                            autoDelete: false,
+                            arguments: null);
+
+    channel.QueueBind(queue:  "chat-eduardoworrel",
+                            exchange: "chat",
+                            routingKey: "");
+                            
+    var consumer = new EventingBasicConsumer(channel);
+   
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine(message);
+    };
+    channel.BasicConsume(queue: "chat-eduardoworrel",
+                            autoAck: true,
+                            consumer: consumer);
+    
+    
+    Console.WriteLine("escreva seu nome");
+    var nome = Console.ReadLine();
+    var message = "";
+    do{
+
+        Console.WriteLine("escreva /sair para sair");
+        message = "["+nome+"]:"+Console.ReadLine();
+        if(message != "/sair"){
+            var body = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(exchange: "chat",
+                            routingKey: "",
+                            basicProperties: null,
+                            body: body);
+        }
+        
+
+    }while(message != "/sair");
+
+}catch(Exception e){
+    Console.WriteLine(e.Message);
+}
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
